feab615a16bb094d54eca17b16516b537ccab872
GitOlli1 <olli.og46@googlemail.com>
2025-06-15 11:42:51 +0200

    update

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import logo from './../public/iptrack_logo.png';
+
+function App() {
+
+  return (
+    <>
+      <div>IpTrack:</div>
+      <img className="logo-size" src={logo}/>
+    </>
+  )
+}
+
+export default App
